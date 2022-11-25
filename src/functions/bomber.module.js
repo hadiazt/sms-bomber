738df976c43bd807706a1d93d1574b8fe31fe048
unknown <hadiazari30@gmail.com>
2022-11-25 13:04:50 +0330

    Update bomber.module.js

--- src/functions/bomber.module.js
+++ src/functions/bomber.module.js
@@ -1,11 +1,28 @@
 // const axios = require('axios')
 // const color = require('chalk')
+const list = [
+    { "a": "b" },
+    { "a": "b" },
+    { "a": "b" },
+    { "a": "b" },
+    { "a": "b" },
 
+
+]
 module.exports = ({ number }) => {
 
     const validate = number.search(/^9\d{9}$/i);
     if (validate !== 0) return console.log("Invalid Format");
 
-    console.log(number);
+    // console.log(number);
+
+
+    setInterval(function () {
+        for (let index = 0; index < list.length; index++) {
+            const element = list[index];
+            console.log(element.a);
+        }
+        }, 30000);
+    // }, 1000);
 
 }

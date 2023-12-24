5038a1babce3afd31d2fefd2524a742bc01d5e3c
hadi <hadiazari30@gmail.com>
2023-12-24 15:04:11 +0330

    Update API endpoints for OTP services

--- src/functions/api.module.js
+++ src/functions/api.module.js
@@ -4,44 +4,31 @@ module.exports = ({ number }) => {
     list = [
         {
             "url": "https://api.digikala.com/v1/user/authenticate/",
-            "data": { "username": '0' + number },
+            "data": { "username": '0' + number, "force_send_otp": true, "otp_call": true },
             "name": "DigiKala",
             "type": "WEB",
             "method": "POST"
         },
         {
-            "url": "https://shadmessenger12.iranlms.ir",
-            "data": {
-                "api_version": "3",
-                "method": "sendCode",
-                "data": {
-                    "phone_number": `+98${number}`,
-                    "send_type": "SMS"
-                }
-            },
-            "name": "Shad",
-            "type": "WEB",
-            "method": "POST"
-        },
-        {
-            "url": "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp",
+
+            "utl": `https://digitalsignup.snapp.ir/ds3/api/v3/otp?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu&cellphone=0${phone}`,
             "data": { cellphone: `+98${number}` },
-            "headers": {
-                "Host": "app.snapp.taxi",
-                "content-length": "29",
-                "x-app-name": "passenger-pwa",
-                "x-app-version": "5.0.0",
-                "app-version": "pwa", "user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36",
-                "content-type": "application/json",
-                "accept": "*/*",
-                "origin": "https://app.snapp.taxi",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-dest": "empty",
-                "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"
-            },
+            // "headers": {
+            //     "Host": "app.snapp.taxi",
+            //     "content-length": "29",
+            //     "x-app-name": "passenger-pwa",
+            //     "x-app-version": "5.0.0",
+            //     "app-version": "pwa", "user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36",
+            //     "content-type": "application/json",
+            //     "accept": "*/*",
+            //     "origin": "https://app.snapp.taxi",
+            //     "sec-fetch-site": "same-origin",
+            //     "sec-fetch-mode": "cors",
+            //     "sec-fetch-dest": "empty",
+            //     "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F",
+            //     "accept-encoding": "gzip, deflate, br",
+            //     "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"
+            // },
             "name": "Snapp",
             "type": "WEB",
             "method": "POST"

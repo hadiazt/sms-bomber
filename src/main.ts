aaa0d5349306c9b84cd93c591a95ee5537bbe564
hadi <hadiazari30@gmail.com>
2023-12-24 17:11:51 +0330

    Remove API and bomber modules

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import color from "chalk";
+import promptSync from "prompt-sync";
+import { Validator } from "./functions/validator";
+
+const Entry = promptSync();
+const Number = Entry(
+  color.black.bgYellow("Please Enter Victim Phone Number : ")
+);
+const Loop = Entry(
+  color.black.bgYellow("How Many Message You Want To Send : ")
+);
+
+Validator(Number, Loop)
+  .then((res) => {
+    console.log(color.white.bgGreenBright(res));
+  })
+  .catch((e) => {
+    console.log(color.white.bgRedBright(e));
+  });

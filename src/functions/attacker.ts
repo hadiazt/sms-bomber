be13758dc177d8612968ed8aaeeb339e511500d4
hadi <hadiazari30@gmail.com>
2023-12-24 23:29:16 +0330

    better code

--- src/functions/attacker.ts
+++ src/functions/attacker.ts
@@ -1,35 +1,39 @@
 import color from "chalk";
 import axios from "axios";
 import { API as APIs } from "./api";
+import { Logger } from "./logger";
 
 export const Attacker = async (num: number, Loop: string) => {
   return new Promise((resolve, reject) => {
     // const API = APIs(num)[Math.floor(Math.random() * APIs(num).length)];
-    const API = APIs(num)[3];
+    const API = APIs(num)[5];
     var headers = {};
     if (API.headers) headers = API.headers;
 
     if (API.method == "POST") {
       axios
         .post(API.url, API.data, headers)
-        .then((res) => {
-
-          if (API.name === "DigiKala" && res.data.status === 400) {
-            console.log(
-              color.white.bgGreenBright(
-                `Request From ${API.name} Failed \n${res.data.message}`
-              )
-            );
-          } else if (API.name === "Namava" && !res.data.succeeded) {
-            console.log(
-              color.white.bgGreenBright(
+        .then(
+          (res: {
+            status: number;
+            data: {
+              status: number;
+              message: string;
+              succeeded: boolean;
+              error: { message: string };
+            };
+          }) => {
+            if (API.name === "DigiKala" && res.data.status === 400)
+              reject(`Request From ${API.name} Failed \n${res.data.message}`);
+            else if (API.name === "Namava" && !res.data.succeeded)
+              reject(
                 `Request From ${API.name} Failed \n${res.data.error.message}`
-              )
-            );
-          } else if (res.status === 200) {
-            resolve(`Request From ${API.name} Sends To ${num}`)
+              );
+            //  else if (res.status === 200) {
+            //   resolve(`Request From ${API.name} Sends To ${num}`);
+            // }
           }
-        })
+        )
         .catch((error) => {
           // resolve(`${API.name} : \n${error.response.statusText}`);
           console.log(

2170f7b9cedfbd2ce29ff2664c545e13f0019121
hadi <hadiazari30@gmail.com>
2023-12-24 23:17:51 +0330

    Update validator.ts

--- src/functions/validator.ts
+++ src/functions/validator.ts
@@ -1,24 +1,31 @@
 import { Attacker } from "./attacker";
+import { Logger } from "./logger";
 
 export const Validator = async (Num: string, Loop: string) => {
   return new Promise((resolve, reject) => {
     let Count: number = 0;
 
     if (isNaN(parseFloat(Num))) {
-      reject("Wrong Phone Number Type(Must Be Number)");
+      Logger(true, "ERROR :\nWrong Phone Number Type(Must Be Number)");
     } else if (isNaN(parseFloat(Loop))) {
-      reject("Wrong Loop Type(Must Be Number)");
+      Logger(true, "ERROR :\nWrong Loop Type(Must Be Number)");
     } else {
       if (Num.startsWith("+98")) Num = Num.split("+98")[1];
-      if (Num.startsWith("09")) Num = Num.split("0")[1];
+      else if (Num.startsWith("09")) Num = "9" + Num.split("09")[1];
 
       const loop = setInterval(() => {
-        Count++;
         if (Count === Number(Loop) + 1) {
           clearInterval(loop);
-          resolve("Process Finished");
+          Logger(false, "Process Finished");
         } else {
-          Attacker(Number(Num));
+          Attacker(Number(Num), Loop)
+            .then((res: string) => {
+              Count++;
+              Logger(false, res);
+            })
+            .catch((e: string) => {
+              Logger(true, e);
+            });
         }
       }, 1000);
     }

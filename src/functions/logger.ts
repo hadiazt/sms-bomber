b878e2da485bc5e1c941dbbef00e204ce024caba
hadi <hadiazari30@gmail.com>
2023-12-24 23:17:53 +0330

    Create logger.ts

---
+++ src/functions/logger.ts
@@ -0,0 +1,9 @@
+import color from "chalk";
+
+export const Logger = (Type: boolean, Message: string) => {
+  if (Type) {
+    console.log(color.white.bgRedBright(Message));
+  } else {
+    console.log(color.white.bgGreenBright(Message));
+  }
+};

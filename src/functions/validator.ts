b6a0c59bb2e0faa9bf6c8e079135facfd02c7afe
hadi <hadiazari30@gmail.com>
2023-12-27 19:04:27 +0330

    Update validator.ts

--- src/functions/validator.ts
+++ src/functions/validator.ts
@@ -5,13 +5,12 @@ export const Validator = async (Num: string, Loop: string) => {
   let Count: number = 0;
 
   if (isNaN(parseFloat(Num))) {
-    Logger(true, "ERROR :\nWrong Phone Number Type(Must Be Number)");
+    return Logger(true, "ERROR :\nWrong Phone Number Type(Must Be Number)");
   } else if (isNaN(parseFloat(Loop))) {
-    Logger(true, "ERROR :\nWrong Loop Type(Must Be Number)");
+    return Logger(true, "ERROR :\nWrong Loop Type(Must Be Number)");
+  } else if (Num.startsWith("+98") || Num.startsWith("09")) {
+    return Logger(true, "ERROR :\nPlease Enter number Without 0 Or +98");
   } else {
-    if (Num.startsWith("+98")) Num = Num.split("+98")[1];
-    else if (Num.startsWith("09")) Num = "9" + Num.split("09")[1];
-
     const loop = setInterval(() => {
       if (Count === Number(Loop)) {
         clearInterval(loop);
@@ -20,7 +19,7 @@ export const Validator = async (Num: string, Loop: string) => {
         Attacker(Number(Num), Loop)
           .then((res: string) => {
             Count++;
-            Logger(false, res + `Request : ${Count}`);
+            Logger(false, res + ` Request : ${Count}`);
           })
           .catch((e: string) => {
             Logger(true, e);

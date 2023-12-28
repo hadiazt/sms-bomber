76b8b46a47e126ca32a2e7e0a68da8f3e3245f93
hadi <hadiazari30@gmail.com>
2023-12-28 12:26:18 +0330

    Update logger.ts

--- src/functions/logger.ts
+++ src/functions/logger.ts
@@ -4,6 +4,6 @@ export const Logger = (Type: boolean, Message: string) => {
   if (Type) {
     console.log(color.white.bgRedBright(Message));
   } else {
-    console.log(color.white.bgGreenBright(Message));
+    console.log(color.white.bgGreen(Message));
   }
 };
